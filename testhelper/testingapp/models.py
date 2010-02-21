ffbd912bdd8a3f199054db059fc8c858897e753b
Chris <chris@heisel.org>
2010-02-21 12:00:09 -0500

    Working on object creation tests.

--- testhelper/testingapp/models.py
+++ testhelper/testingapp/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    
+
 class Tag(models.Model):
     name = models.CharField(max_length=255)
-    
+
     class Testing:
         defaults = {}
-    
+
 class Archive(models.Model):
     pass
 
 class Article(models.Model):
     boolean = models.BooleanField()
-    char = models.CharField(blank=True, max_length=255)
+    name = models.CharField(blank=True, max_length=255)
     date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(null=True, blank=True)
     updated_at = models.DateTimeField(null=True, blank=True)
@@ -32,8 +32,10 @@ class Article(models.Model):
     time = models.TimeField(null=True, blank=True)
     url = models.URLField(blank=True)
     xml = models.XMLField(blank=True)
-    
+
     category = models.ForeignKey(Category, related_name="articles", null=True)
     tags = models.ManyToManyField(Tag, related_name="articles", null=True)
     archive = models.OneToOneField(Archive, related_name="article", null=True)
-    
+
+    class Testing:
+        pass
--- testhelper/testingapp/tests.py
+++ testhelper/testingapp/tests.py
@@ -5,13 +5,29 @@ from testhelper.testingapp import models
 class TestHelperTests(DjangoTestCase):
     def test_object_creation_without_defaults(self):
         """
-        Calling create_object should return an instance of the model, with
-        defaults pre-filled from class.Testing.defaults if available.
+        Calling create_object should return an instance of the model even if 
+        it doesn't have a Testing inner class with a defaults property.
         """
         t = self.create_object(models.Tag)
-        self.assertEqual('', t.name)
+        self.assert_(isinstance(t, models.Tag))
 
         c = self.create_object(models.Category)
-        self.assertEqual('', c.name)
+        self.assert_(isinstance(c, models.Category))
 
+    def test_object_creation_with_defaults(self):
+        """
+        If an object contains an inner class Testing with defaults property
+        then the instance returned should use that default.
+        """
+        defaults = { 'name': 'Space Pilot 3000' }
+        models.Article.Testing.defaults = defaults
+
+        a = self.create_object(models.Article)
+        self.assertEqual(defaults['name'], a.name)
+
+    def test_object_creation_with_default_relations(self):
+        """
+        Default relations should be able to be specified
+        """
+        
         
